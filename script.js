e63724b5049c53770ba14096c8f4066afc279504
Jennifer Duff <jduffbowser@gmail.com>
2023-11-14 18:01:50 -0800

    shows/hides mobile nav when clicking mobile menu icon

--- script.js
+++ script.js
@@ -1,19 +1,30 @@
 let menuIcon = document.getElementById("menu-icon");
-console.log(menuIcon);
-
 let nav = document.getElementsByTagName("nav")[0];
 
 // Initial setup of menu
 let windowWidth = window.innerWidth;
-showMenuIcon(windowWidth);
+showHideMenuIcon(windowWidth);
 
 // Monitor for updates
 window.addEventListener("resize", () => {
     windowWidth = window.innerWidth;
-    showMenuIcon(windowWidth);
+    showHideMenuIcon(windowWidth);
 });
 
-function showMenuIcon(windowWidth) {
+// Handle clicks on/off the mobile menu/navigation
+document.addEventListener("click", (event) => {
+    console.log(event.target);
+    console.log(nav.contains(event.target));
+    if (event.target === menuIcon) {
+        showMobileNav();
+    }
+    else if (!nav.contains(event.target)) {
+        console.log('hiding!');
+        hideMobileNav();
+    }
+});
+
+function showHideMenuIcon(windowWidth) {
     if (windowWidth <= 675) {
         menuIcon.style.display = "inline-block";
         nav.classList.add("visually-hidden");
@@ -23,3 +34,14 @@ function showMenuIcon(windowWidth) {
         nav.classList.remove("visually-hidden");
     }
 }
+
+function showMobileNav() {
+    nav.classList.add('mobile-nav');
+    nav.classList.remove("visually-hidden");
+}
+
+function hideMobileNav() {
+    nav.classList.add("visually-hidden");
+    nav.classList.remove('mobile-nav');
+}
+
