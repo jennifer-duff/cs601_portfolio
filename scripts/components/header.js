1d9ecfa0fc0501cae898298e638f971aefe48e3f
Jennifer Duff <jduffbowser@gmail.com>
2023-11-25 18:45:21 -0800

    makes '<head>' a Vue component

--- scripts/components/header.js
+++ scripts/components/header.js
@@ -1,5 +1,18 @@
 const header = Vue.createApp({
     el: "#header",
+    template: `
+        <a href="index.html" class="logo">
+            <img src="./assets/logos/full_logo.svg" alt="JDB Development logo">
+        </a>
+        <img id="menu-icon" src="./assets/menu_icon.svg" alt="menu">
+        <nav>
+            <ul class="menu">
+                <li v-for="(href, pageName) in pages"><a :class="findCurrentPage(pageName)"
+                        :href="href">{{pageName}}</a>
+                </li>
+            </ul>
+        </nav>
+    `,
     data() {
         return {
             currentPage: "home",
@@ -23,7 +36,5 @@ const header = Vue.createApp({
         }
     }
 });
-
-
 
 header.mount("#header");
