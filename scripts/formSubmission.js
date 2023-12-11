37526bda2bff04404e9db4ae1b26bb55ce371009
Jennifer Duff <jduffbowser@gmail.com>
2023-12-10 18:26:01 -0800

    comments + styling tweaks

--- scripts/formSubmission.js
+++ scripts/formSubmission.js
@@ -2,6 +2,8 @@ const form = document.forms["contact-form"];
 const formWrapper = document.getElementById("form-wrapper");
 
 form.addEventListener("submit", (event) => {
+    // I've purposfully stopped form submission here, since this site is live 
+    // and I don't want people to actuallyy be able to contact me
     event.preventDefault();
     formWrapper.innerHTML = "<p id='success-msg'>Thanks for your message!</p>"
 });
